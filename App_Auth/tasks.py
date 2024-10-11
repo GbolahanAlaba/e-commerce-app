7948bc350cb2c644f2ae424dfb70c31375138509
gbolahan <thegbolahanalaba@gmail.com>
2024-10-11 20:13:32 +0100

    Code structure updated

--- App_Auth/tasks.py
+++ App_Auth/tasks.py
@@ -2,9 +2,6 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from . utils import *
 
-@shared_task
-def add(x, y):
-    return x + y
 
 @shared_task
 def send_activation_email(user, otp):
