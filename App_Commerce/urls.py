87f1f1786c51040216a05c958541c0324a178ae7
gbolahan <thegbolahanalaba@gmail.com>
2024-10-11 21:14:32 +0100

    Category & Subcategory added

--- App_Commerce/urls.py
+++ App_Commerce/urls.py
@@ -7,5 +7,8 @@ from knox import views as knox_views
 
 urlpatterns = [
    path('create-category/', CategoryViewSets.as_view({"post": "create_category"}), name='category-create'),
+   path('list-category/', CategoryViewSets.as_view({"post": "list_category"}), name='category-list'),
+   path('create-subcategory/', CategoryViewSets.as_view({"post": "create_subcategory"}), name='sucategory-create'),
+   path('list-category/', CategoryViewSets.as_view({"post": "list_category"}), name='subcategory-list'),
   
 ]
